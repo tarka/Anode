b1833fa79a40c041b8a0f792fe7ff7e59f50fbfd
Steve Smith <tarkasteve@gmail.com>
2010-05-08 17:58:39 +1000

    Add JDEE project config

---
+++ prj.el
@@ -0,0 +1,9 @@
+(jde-project-file-version "1.0")
+(jde-set-variables
+ '(jde-build-function '(jde-ant-build))
+ '(jde-ant-args "-emacs compile reinstall")
+ '(jde-enable-abbrev-mode t)
+ '(jde-global-classpath 
+   '("/usr/share/java/ant.jar"
+     "./libs"
+     "~/software/android/platforms/android-7/android.jar")))
