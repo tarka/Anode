8e0247cea1140066e72c2def48660fb79284bcc4
Steve Smith <tarkasteve@gmail.com>
2010-05-12 17:10:01 +1000

    Start of widget; imported 9patch background from Music app.

---
+++ src/net/haltcondition/anode/Updater.java
@@ -0,0 +1,10 @@
+package net.haltcondition.anode;
+
+import android.appwidget.AppWidgetProvider;
+
+/**
+ * Copyright Steve Smith (tarkasteve@gmail.com): 12/05/2010
+ */
+public class Updater extends AppWidgetProvider
+{
+}
