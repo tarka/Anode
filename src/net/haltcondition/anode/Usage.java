d6fb36442358eb136dd292a2861d2c9698383a3b
Steve Smith <tarkasteve@gmail.com>
2010-05-16 18:34:36 +1000

    Add usage params and conversions

--- src/net/haltcondition/anode/Usage.java
+++ src/net/haltcondition/anode/Usage.java
@@ -1,8 +1,52 @@
 package net.haltcondition.anode;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Copyright Steve Smith (tarkasteve@gmail.com): 16/05/2010
  */
 public class Usage
 {
+    private Long totalQuota;
+    private Date rollOver;
+    private Long used;
+
+    private SimpleDateFormat dateParser = new SimpleDateFormat("yyyy-MM-dd");
+
+    public Usage()
+    {
+    }
+
+    public Long getTotalQuota()
+    {
+        return totalQuota;
+    }
+
+    public void setTotalQuota(String str)
+    {
+        this.totalQuota = Long.valueOf(str);
+    }
+
+    public Date getRollOver()
+    {
+        return rollOver;
+    }
+
+    public void setRollOver(String str)
+        throws ParseException
+    {
+        rollOver = dateParser.parse(str);
+    }
+
+    public Long getUsed()
+    {
+        return used;
+    }
+
+    public void setUsed(String str)
+    {
+        this.used = Long.valueOf(used);
+    }
 }
