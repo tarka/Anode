9cbbc8ea1b580a9c51aca105659ca4d01fe6838f
Steve Smith <tarkasteve@gmail.com>
2010-05-17 14:47:26 +1000

    Get TestNG test working

--- tests/net/haltcondition/anode/UsageTest.java
+++ tests/net/haltcondition/anode/UsageTest.java
@@ -1,27 +1,34 @@
 package net.haltcondition.anode;
 
-import junit.framework.TestCase;
-import net.haltcondition.anode.Usage;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.fail;
 
-import java.lang.Exception;
-import java.lang.Override;
+import java.text.ParseException;
 import java.util.Calendar;
 
+@Test
 public class UsageTest
-    extends TestCase
 {
     private Usage usage;
 
-    @Override
-    protected void setUp() throws Exception
+    @BeforeMethod
+    protected void setUp()
     {
         usage = new Usage();
     }
 
+    @Test
     void testDateParse(){
-        usage.setRollOver("2000-05-21");
+        try {
+            usage.setRollOver("2000-12-21");
+        } catch (ParseException e) {
+            fail(e.getMessage(), e);
+        }
         assertEquals(usage.getRollOver().get(Calendar.YEAR), 2000);
-        assertEquals(usage.getRollOver().get(Calendar.MONTH), 5);
+        assertEquals(usage.getRollOver().get(Calendar.MONTH), Calendar.DECEMBER);
         assertEquals(usage.getRollOver().get(Calendar.DAY_OF_MONTH), 21);
     }
 }
