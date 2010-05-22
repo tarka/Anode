2e33a04486cffb7ab410c97ddd7ff82fe0170c7e
Steve Smith <tarkasteve@gmail.com>
2010-05-22 15:27:42 +1000

    Clean up imports

--- src/net/haltcondition/anode/SettingsHelper.java
+++ src/net/haltcondition/anode/SettingsHelper.java
@@ -1,14 +1,8 @@
 
 package net.haltcondition.anode;
 
-import android.content.ContentValues;
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.database.Cursor;
-import android.database.SQLException;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
-import android.util.Log;
 
 
 public class SettingsHelper
