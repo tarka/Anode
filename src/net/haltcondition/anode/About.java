299c5a94f9c338c682459a63971bb6b5cfcb239c
Steve Smith <tarkasteve@gmail.com>
2010-05-30 17:48:03 +1000

    Add basic About screen

---
+++ src/net/haltcondition/anode/About.java
@@ -0,0 +1,16 @@
+package net.haltcondition.anode;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Copyright Steve Smith (tarkasteve@gmail.com): 30/05/2010
+ */
+public class About extends Activity
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+}
